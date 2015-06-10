88464b1d7b9074da5c415247fcd0a10cc648e983
Beatris <betty@localhost.localdomain>
2015-06-10 13:51:20 +0300

    Add the initial skeleton of the project classes.

---
+++ HelpHass/fastest_route_calculator.py
@@ -0,0 +1,21 @@
+class FastestRouteCalculator():
+
+    def __init__(self, graph, start, end, path=[]):
+        self.graph = graph
+        self.start = start
+        self.end = end
+        self.path = path
+
+    def find_all_routes(self):
+        '''
+        Find all possible paths between
+        two nodes in a graph.
+        '''
+        pass
+
+    def find_fastest_route(self):
+        '''
+        Find the shortest path from all
+        possible paths between nodes in a graph.
+        '''
+        pass
---
+++ HelpHass/graph_constructor.py
@@ -0,0 +1,12 @@
+class GraphConstructor():
+
+    def __init__(self, nodes, arcs):
+        self.nodes = nodes
+        self.arcs = arcs
+
+    def construct_graph(self):
+        '''
+        Construct a graph in a form of list
+        by given nodes and arcs.
+        '''
+        pass
