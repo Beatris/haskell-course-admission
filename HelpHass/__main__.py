668e37c6d118258e350596ff00b6fd0c78988d56
beatris <beatrisvboneva@gmail.com>
2015-06-10 14:56:42 +0300

    The graph construction functionality added.

--- HelpHass/__main__.py
+++ HelpHass/__main__.py
@@ -1,8 +1,19 @@
-input_list = []
+from graph_constructor import GraphConstructor
 
+
+routes = []
+
+# Procces the input:
+print("Input:")
 while True:
     input_str = raw_input("> ")
+    route = input_str.split(" ")
     if input_str == "":
         break
     else:
-        input_list.append(input_str)
+        routes.append(route)
+
+# Construct the graph:
+graph_constructor = GraphConstructor(routes)
+graph = graph_constructor.construct_graph()
+
--- HelpHass/graph_constructor.py
+++ HelpHass/graph_constructor.py
@@ -1,12 +1,21 @@
 class GraphConstructor():
 
-    def __init__(self, nodes, arcs):
-        self.nodes = nodes
-        self.arcs = arcs
+    def __init__(self, routes):
+        self.routes = routes
 
     def construct_graph(self):
         '''
         Construct a graph in a form of list
-        by given nodes and arcs.
+        by given routes.
         '''
-        pass
+        graph = {}
+        for route in self.routes:
+            node = route[0]
+            arc = route[1]
+            if node not in graph.keys():
+                graph[node] = arc
+            else:
+                graph[node] = list(graph[node])
+                graph[node].append(arc)
+
+        return graph
