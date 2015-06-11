28e809f7a37f07276d418398b200cfd90f273cb3
beatris <beatrisvboneva@gmail.com>
2015-06-11 17:30:47 +0300

    Add the initial file structure of the module.

---
+++ LanguageGame/__main__.py
@@ -0,0 +1,21 @@
+# Procces the input:
+print "Input:"
+while True:
+    input_str = raw_input()
+    route = input_str.split(" ")
+    if input_str == "":
+        break
+    else:
+        routes.append(route)
+
+# Construct the graph:
+graph_constructor = GraphConstructor(routes)
+graph = graph_constructor.construct_graph()
+
+# Find the shortest path between given nodes:
+fastest_route_calculator =\
+    FastestRouteCalculator(graph, START_STATION, END_STATION)
+
+fastest_route = fastest_route_calculator.find_fastest_route()
+print "Output:"
+print ' '.join(fastest_route)
