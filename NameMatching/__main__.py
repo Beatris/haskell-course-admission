dd90e0ab295a6f6021b96665cd174aecc000a83c
bboneva <beatrisvboneva@gmail.com>
2015-06-13 20:02:16 +0300

    Add the working solution for the NameMatching task.

--- NameMatching/__main__.py
+++ NameMatching/__main__.py
@@ -4,11 +4,12 @@ from chance_calculator import ChanceCalculator
 while True:
     print "Input:"
     known_persons = raw_input().split()
-    if known_persons == "":
+    if known_persons == []:
         break
     names = raw_input().split()
-    if names == "":
+    if names == []:
         break
-    chance_calculator = ChanceCalculator(known_persons, names)
+    chance_calculator = ChanceCalculator(known_persons, set(names))
     print "Output:"
     print chance_calculator.mistake_percentage
+    print '-' * 20
--- NameMatching/chance_calculator.py
+++ NameMatching/chance_calculator.py
@@ -1,11 +1,19 @@
 class ChanceCalculator:
 
-	def __init__(self, known_persons, names):
-		self.female_names = filter(lambda x: "tta" == x[-3:], names)
-		self.male_names = filter(lambda x: "ss" == x[-2:], names)
-		self.known_females = int(known_persons[1])
-		self.known_males = int(known_persons[0])
-		self.mistake_percentage = "{0}%".format(int(self.calculate_chance() * 100))
+    def __init__(self, known_persons, names):
+        self.female_names = filter(lambda x: "tta" == x[-3:], names)
+        self.male_names = filter(lambda x: "ss" == x[-2:], names)
+        self.known_females = int(known_persons[1])
+        self.known_males = int(known_persons[0])
+        self.female_chance =\
+            self.calculate_chance(self.known_females, self.female_names)
+        self.male_chance =\
+            self.calculate_chance(self.known_males, self.male_names)
+        self.average_chance = self.female_chance * self.male_chance
+        self.mistake_percentage = "{0}%".format(int(self.average_chance * 100))
 
-	def calculate_chance(self):
-		pass
+    def calculate_chance(self, known_persons, names):
+        unknown_persons = len(names) - known_persons
+        if unknown_persons <= 0:
+            unknown_persons = 1
+        return 1.0 / unknown_persons
